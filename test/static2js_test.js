e600dd16aa5ae523d9173c03e43cc0fe1fa90bec
Philip Sampaio Silva <philip.sampaio@gmail.com>
2016-06-20 02:34:11 -0300

    Add more tests

--- test/static2js_test.js
+++ test/static2js_test.js
@@ -20,4 +20,20 @@ describe('Static2JS', () => {
         expect(err).to.eql(new TypeError());
       });
   });
+
+  it('matches the some patterns for static files', () => {
+    let proto = Static2JS.prototype;
+
+    expect(proto.pattern.test("foo.static")).to.be.ok;
+    expect(proto.pattern.test("bar.static.html")).to.be.ok;
+    expect(proto.pattern.test("baz.static.txt")).to.be.ok;
+    expect(proto.pattern.test("baz.stati.txt")).to.be.false;
+  });
+
+  it('is a javascript plugin for Brunch', () => {
+    let proto = Static2JS.prototype;
+
+    expect(proto.brunchPlugin).to.be.true;
+    expect(proto.type).to.eql('javascript');
+  });
 });
